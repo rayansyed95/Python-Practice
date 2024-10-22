8c5e1d2341d0b44bf52fcf92a11d117ee670bc39
rayansyed95 <22228355@bucks.ac.uk>
2024-10-22 21:28:28 +0100

    first commit

---
+++ practice.py
@@ -0,0 +1,137 @@
+#Lecture 2
+# val = 5
+# val *= 5 
+# print (val)
+
+# tab = 5
+# for i in range(1, 11):
+#     print(tab, " x ", i, " = ", tab*i)
+
+# Assignment lecture 2
+# table = int(input("which table do you want me to display:"))
+# lower = int(input("write what number you want the table to start with:"))
+# higher = int(input("Write what number you want the table to end with:"))
+# for i in range (lower, higher):
+#     print (table, " x ", i, " = ", table*i)
+
+# Lecture 2 assignment
+# table = int(input("Enter a number: "))
+# lower = int(input("Enter a low number: "))
+# higher = int(input("Enter a high number: "))
+# for i in range (lower, higher): 
+#     print(table, " x ", i, " = ", table*i)
+#     i += 1
+
+#Lecture 3 assign 1
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter another number: "))
+
+# if num1 > num2 :
+#     print (num1)
+# else:
+#     print (num2)
+
+
+#Lecture 3 assign 2
+# cities = ["Lahore", "Karachi", "Islamabad", "Multan", "Faisalabad" ]
+# my_city = (input("Enter your city: "))
+# if my_city in cities:
+#     print ("city found")
+# else:
+#     print ("could not find your city")   
+
+
+#Lecture 3 assign 3
+# username = "manager" or "executive"
+# password = "nothing"
+# if username == "manager" or "executive" and password == "nothing":
+#     print ("Login Successful!")
+# else:
+#     print ("Login Unsucessful")  
+
+
+
+"""
+Started the assignments again 26/04/2024:
+"""
+
+# num1 = 29
+# num2 = 5
+# div = num1 / num2
+# print ("%.5f" %div) 
+
+# user = input("Enter Your name:")
+# age = int(input("Enter your age:"))
+# print ("Welcome ", user)
+# print ("Your age is", age)
+
+# table = int(input ("Enter the time table want displayed:"))
+# Lower = int (input("Enter the number you want the table to start with:"))
+# Higher = int(input("Enter the number you want the table to end with:"))
+# for i in range (Lower, Higher):
+#     print(table, " x ", i, " = ", table*i)
+
+#Lecture 3 Assignment #1
+
+# num1 = input ("Enter a number: ")
+# num2 = input ("Enter another number: ")
+# if (num1 > num2):
+#     print (num1)
+# else:
+#     print (num2)
+
+#Lecture 3 Assignment #2
+
+# cities = ["karachi", "london", "lahore", "islamabad", "birmingham"]
+# my_city = input ("Enter your city: ")
+# if my_city in cities:
+#     print ("Found city")
+# else:
+#     print ("Sorry city not found")    
+
+#Lecture 3 Assignment #3 (ask about this question)
+
+# username = "manager"
+# password = "executive"
+# if username == "manager" or username == "executive" and password != "nothing ":
+#     print ("login successful")
+# else:
+#     print ("login unsuccessful")    
+
+# Extra Questions: #4
+
+# mob_num = "12345"
+# my_num = input("Enter your mobile number: ")
+# if my_num == mob_num:
+#     print ("Correct")
+# else:
+#     print ("Incorrect")    
+
+# Q5
+
+# num1 = float(input("Enter a number: "))
+# num2 = float(input("Enter a second number: "))
+# num3 = float(input("Enter a third number: "))
+
+# if num1 < num2 and num1 < num3:
+#     print("The smallest number is:", num1)
+# elif num2 < num1 and num2 < num3:
+#     print("The smallest number is:", num2)
+# else:
+#     print("The smallest number is:", num3)
+
+# Q6
+
+name = input("Enter your name: " )
+password = input("Enter your password: ")
+petname = input("Enetr your pets name: ")
+
+if name == ("rayan") and password == ("12345") and petname ==("mano"):
+    print ("Welcome Rayan Syed how may i help you")
+else:
+    print ("You've entered Incorrect Details please try again.")    
+
+
+
+
+
