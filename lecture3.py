cadb412c09fadfcb6a07b65ae0af2a1da01dddc0
rayansyed95 <22228355@bucks.ac.uk>
2024-10-23 22:21:15 +0100

    lecture 3

---
+++ lecture3.py
@@ -0,0 +1,17 @@
+# Exercise 1
+area_of_circle: float = 3.14 * 5 ** 2
+print (area_of_circle)
+
+# Exercise 2
+numbers = 10
+if numbers %2 == 0:
+    print ("Even number")
+else:
+    print ("Odd number") 
+
+# Exercise 3
+age = 60
+if age >= 60:
+    print ("You're eligible")
+else:
+    print ("You're not eligible")
