1adb7a7a42ff7ee7db9701dd8cfbab475aaf96ec
rayansyed95 <22228355@bucks.ac.uk>
2024-10-23 14:13:46 +0100

    lecture 2

---
+++ lecture2.py
@@ -0,0 +1,23 @@
+# Exercise 1
+favourite_colour: str = ("black")
+
+# Exercise 2
+number = 100
+number = float(number)
+print (number)
+
+# Exercise 3
+favourite_fruits = ["orange", "banana", "grapes"]
+print (favourite_fruits)
+
+# Exercise 4
+student = {"name": "Rayan", "age": 29, "grade": "A*"}
+print (student)
+
+# Exercise 5
+week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+# Exercise 6
+x = 5 * 5
+print (x)
+
